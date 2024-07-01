e17c6446d17945b7928ca6024301574af76a6e6a
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-07-01 18:25:02 -0400

    done

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -23,7 +23,24 @@ public class LogginLabTest {
 
         for (Integer i = 1; i <= finalLimit; i++) {
             if (lab.thresholdExceeds(i)) {
-                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                logger.log(Level.INFO, "Threshold not reached! It is " + i);
+                assertTrue(lab.thresholdExceeds(i));
+            } else {
+                logger.log(Level.INFO, "Threshold finally reached!");
+                assertFalse(lab.thresholdExceeds(i));
+            }
+        }
+    }
+
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i >= finalLimit; i++) {
+            if (lab.thresholdExceeds(i)) {
+                logger.log(Level.INFO, "Threshold not reached! It is " + i);
                 assertTrue(lab.thresholdExceeds(i));
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
